396a2b5abb8624c3f9ad35a7e58d2fb1aed906a4
Fan Yang <fan.yang@cs.kuleuven.be>
2016-05-11 14:20:14 +0200

    change back to _

--- Parcel.py
+++ Parcel.py
@@ -227,9 +227,9 @@ class Parcel:
 
 class ParcelsHandler:
     def __init__(self, parcels):
-        self.available-parcels = parcels
-        self.on-the-road-parcels = []
-        self.delivered-parcels = []
+        self.available_parcels = parcels
+        self.on_the_road_parcels = []
+        self.delivered_parcels = []
 
 
     def claim_parcel(self, team_id, parcel_id):
@@ -237,18 +237,18 @@ class ParcelsHandler:
         for parcel in self.available_parcels:
             if parcel.parcel_id == parcel_id and not parcel.has_team(team_id):
                 parcel.add_team_id(team_id)
-                self.available-parcels.remove(parcel)
-                self.on-the-road-parcels.append(parcel)
+                self.available_parcels.remove(parcel)
+                self.on_the_road_parcels.append(parcel)
                 ret = True
 
         return ret
 
     def delivered(self, team_id, parcel_id):
         ret = False
-        for parcel in self.on-the-road-parcels:
+        for parcel in self.on_the_road_parcels:
             if parcel.parcel_id == parcel_id and parcel.has_team(team_id):
-                self.on-the-road-parcels.remove(parcel)
-                self.delivered-parcels.append(parcel)
+                self.on_the_road_parcels.remove(parcel)
+                self.delivered_parcels.append(parcel)
                 ret = True
 
         return ret
